2b830bbf03fc8bd209372f34a1c79d0572d49e72
Informatica Arelux <informatica@arelux.com>
2020-04-02 14:03:24 +0200

    arelux_automation (básico)

---
+++ arelux_automation/models/stock_picking.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models, api, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+    
+    @api.one    
+    def action_send_account_invoice_out_refund(self):
+        return_object = super(StockPicking, self).action_send_account_invoice_out_refund()
+        #save_log
+        automation_log_vals = {                    
+            'model': 'account.invoice',
+            'res_id': self.out_refund_invoice_id.id,
+            'category': 'account_invoice',
+            'action': 'create',                                                                                                                                                                                           
+        }
+        automation_log_obj = self.env['automation.log'].sudo().create(automation_log_vals)
+        #return
+        return return_object
