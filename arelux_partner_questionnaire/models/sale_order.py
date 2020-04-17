d7eb50912e92f01bf6a2d2a2a66750e1041a209f
Informatica Arelux <informatica@arelux.com>
2020-04-17 13:34:24 +0200

    fix create

--- arelux_partner_questionnaire/models/sale_order.py
+++ arelux_partner_questionnaire/models/sale_order.py
@@ -27,63 +27,30 @@ class SaleOrder(models.Model):
         string='Tipo de cliente',
     )
     
-    @api.multi
-    @api.onchange('partner_id')
-    def onchange_partner_id(self):
-        """
-        Update the following fields when the partner is changed:
-        - Pricelist
-        - Payment term
-        - Invoice address
-        - Delivery address
-        """
-        if not self.partner_id:
-            self.update({
-                'partner_invoice_id': False,
-                'partner_shipping_id': False,
-                'payment_term_id': False,
-                'fiscal_position_id': False,
-            })
-            return
-
-        addr = self.partner_id.address_get(['delivery', 'invoice'])
-        values = {
-            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
-            'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
-            'partner_invoice_id': addr['invoice'],
-            'partner_shipping_id': addr['delivery'],
-            'note': self.with_context(lang=self.partner_id.lang).env.user.company_id.sale_note,
-        }
-        
-        if self.opportunity_id.id==0:
-            values['ar_qt_activity_type'] = self.partner_id.ar_qt_activity_type or False
-            values['ar_qt_customer_type'] = self.partner_id.ar_qt_customer_type or False
-        else:
-            values['ar_qt_activity_type'] = self.opportunity_id.ar_qt_activity_type or False
-            values['ar_qt_customer_type'] = self.opportunity_id.ar_qt_customer_type or False            
-
-        if self.partner_id.user_id.id>0:
+    @api.onchange('partner_shipping_id')
+    def onchange_partner_shipping_id(self):
+        if self.partner_id.id>0:
             if self.opportunity_id.id==0:
-                values['user_id'] = self.partner_id.user_id.id
-            else:
-                values['user_id'] = self.opportunity_id.user_id.id
-        
+                self.ar_qt_activity_type = self.partner_id.ar_qt_activity_type
+                self.ar_qt_customer_type = self.partner_id.ar_qt_customer_type
+                
+    @api.onchange('opportunity_id')
+    def onchange_opportunity_id(self):
         if self.opportunity_id.id>0:
-            values['team_id'] = self.opportunity_id.team_id.id
-                    
-        self.update(values)
+            self.ar_qt_activity_type = self.opportunity_id.ar_qt_activity_type
+            self.ar_qt_customer_type = self.opportunity_id.ar_qt_customer_type                        
         
     @api.model
     def create(self, values):   
         return_val = super(SaleOrder, self).create(values)
         #operations
-        if self.opportunity_id.id>0:
-            #ar_qt_activity_type                    
-            if self.opportunity_id.ar_qt_activity_type!=False and self.opportunity_id.ar_qt_activity_type!=self.ar_qt_activity_type:
-                self.ar_qt_activity_type = self.opportunity_id.ar_qt_activity_type
-            #ar_qt_customer_type                    
-            if self.opportunity_id.ar_qt_customer_type!=False and self.opportunity_id.ar_qt_customer_type!=self.ar_qt_customer_type:
-                self.ar_qt_customer_type = self.opportunity_id.ar_qt_customer_type
+        if return_val.opportunity_id.id>0:
+            #ar_qt_activity_type
+            if return_val.ar_qt_activity_type==False:
+                return_val.ar_qt_activity_type = return_val.opportunity_id.ar_qt_activity_type
+            #ar_qt_customer_type
+            if return_val.ar_qt_customer_type==False:
+                return_val.ar_qt_customer_type = return_val.opportunity_id.ar_qt_customer_type
         #return
         return return_val
         
