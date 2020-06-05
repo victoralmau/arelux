29481ab8605c7a2df6705411b3e9e5fb13afbec6
Informatica Arelux <informatica@arelux.com>
2020-06-05 14:16:23 +0200

    mejoras

--- arelux_quality_forms/models/__init__.py
+++ arelux_quality_forms/models/__init__.py
@@ -3,6 +3,7 @@ from . import fire_drill
 from . import fire_drill_decision
 from . import maintenance_installation
 from . import maintenance_installation_need_check
+from . import quality_team
 from . import waste_remove
 from . import waste_remove_detail
 from . import waste_remove_product
