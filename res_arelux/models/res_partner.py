b2b62f5632f0bbe710b6a67d44507c23ceff75f1
victoralmau <victoralmau@gmail.com>
2020-08-17 11:04:55 +0200

    EmailAddress

--- res_arelux/models/res_partner.py
+++ res_arelux/models/res_partner.py
@@ -2,7 +2,7 @@
 from odoo import api, models, fields, _
 from odoo.exceptions import Warning as UserError
 from odoo.exceptions import ValidationError
-from validate_email import validate_email
+from validate_email.email_address import EmailAddress
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -63,10 +63,7 @@ class ResPartner(models.Model):
         if allow_write:
             if 'email' in vals:
                 if vals['email'] != '':
-                    is_valid = validate_email(
-                        email_address=vals['email'],
-                        check_regex=True
-                    )
+                    is_valid = EmailAddress(vals['email'])
                     if not is_valid:
                         allow_write = False
                         raise ValidationError(_('Email incorrect'))
