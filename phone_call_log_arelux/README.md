e5edb8829d62359d2d7e9ff1bba4beeb75897806
victoralmau <victoralmau@gmail.com>
2020-08-26 11:23:13 +0200

    phone_call_log_arelux

---
+++ phone_call_log_arelux/README.md
@@ -0,0 +1,1 @@
+Optimiza el proceso del addon phone_call_log para auto-marcar los leads como repasados.
---
+++ phone_call_log_arelux/models/crm_lead.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    is_revision = fields.Boolean(
+        string='Is revision?',
+        default=False,
+        readonly=True
+    )
