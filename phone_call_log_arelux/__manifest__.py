facf7e70868ae98b82386ea57002f3a86e8a7f75
victoralmau <victoralmau@gmail.com>
2020-08-26 11:30:10 +0200

    Update __manifest__.py

--- phone_call_log_arelux/__manifest__.py
+++ phone_call_log_arelux/__manifest__.py
@@ -1,18 +1,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    'name': 'Phone Call Log Arelux',
-    'version': '10.0.1.0.0',
-    'author': 'Odoo Nodriza Tech (ONT)',
-    'website': 'https://nodrizatech.com/',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'depends': [
-        'phone_call_log',  # https://github.com/OdooNodrizaTech/tools
-        'arelux_partner_questionnaire',
-        'shipping_expedition',  # https://github.com/OdooNodrizaTech/stock
-        'crm'
+    "name": "Phone Call Log Arelux",
+    "version": "12.0.1.0.0",
+    "author": "Odoo Nodriza Tech (ONT), "
+              "Odoo Community Association (OCA)",
+    "website": "https://nodrizatech.com/",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "phone_call_log",  # https://github.com/OdooNodrizaTech/tools
+        "arelux_partner_questionnaire",
+        "shipping_expedition",  # https://github.com/OdooNodrizaTech/stock
+        "crm"
     ],
-    'data': [],
-    'installable': True,
-    'auto_install': False
+    "data": [],
+    "installable": True,
+    "auto_install": False
 }
